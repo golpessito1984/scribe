396dd1f904ce3b46e085ce8711bb31dcb81e891c
David Ruiz de la Rosa <david.ruizdelarosa@gmail.com>
2020-05-26 16:28:14 +0200

    Fix rspec

--- spec/features/users/user_can_sign_up_spec.rb
+++ spec/features/users/user_can_sign_up_spec.rb
@@ -7,7 +7,7 @@ feature 'user can sign_up' do
     fill_in 'Name', with: 'David'
     fill_in 'Surname', with: 'Ruiz de la Rosa'
     fill_in 'Password', with: '918912178'
-    fill_in 'Password confirmation', with: '918912178'
+    fill_in 'Password Confirmation', with: '918912178'
     click_on 'Sign Up'
     expect(page).to have_css '#user p', text: 'David'
     expect(page).to have_css '#user p', text: 'Ruiz de la Rosa'
