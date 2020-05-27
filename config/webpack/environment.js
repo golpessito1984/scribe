5333f566096c33a67727197636cdb3dfb5236978
David Ruiz de la Rosa <david.ruizdelarosa@gmail.com>
2020-05-27 20:19:30 +0200

    Fix bootstrap templates

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,12 +4,20 @@ const webpack = require("webpack")
 
 environment.plugins.append("Provide", new webpack.ProvidePlugin({
 
-    $: 'jquery',
-
-    jQuery: 'jquery',
+    // $: 'jquery',
+    $: 'jquery/src/jquery',
+    // jQuery: 'jquery',
+    jQuery: 'jquery/src/jquery',
 
     Popper: ['popper.js', 'default']
 
 }))
+
+// environment.plugins.prepend('Provide',
+//     new webpack.ProvidePlugin({
+//         $: 'jquery/src/jquery',
+//         jQuery: 'jquery/src/jquery'
+//     })
+// )
 
 module.exports = environment
